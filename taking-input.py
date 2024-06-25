fbe52928f8098ce93a1e12b790929b6388f0fc34
luckkalpna <luckkalpna@gmail.com>
2024-06-25 15:14:55 +0530

    Taking User Input

--- taking-input.py
+++ taking-input.py
@@ -1,9 +1,13 @@
 a = input("Enter your name: ")
 print("My name is:", a)
 
+# Concatenate two strings
+
 x = input("Enter first number:")
 y = input("Enter second number:")
 print(x + y)
+
+# Change any string to integer
 
 x = input("Enter first number:")
 y = input("Enter second number:")
