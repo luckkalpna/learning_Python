507c1274eb2d09eef9c224c52513163a82748d37
luckkalpna <luckkalpna@gmail.com>
2024-06-26 13:45:23 +0530

    If Else Loops & Elif statement

---
+++ loops.py
@@ -0,0 +1,29 @@
+
+# ------------------If Else statement---------------------
+
+# a = int(input("Enter your age: "))
+# print("Your age is:", a)
+
+# if(a>18):
+#     print("You can drive")
+# else:
+#     print("You can't drive")
+
+# ----------------If Else statement with elif------------------
+
+num = int(input("Enter the value of number: "))
+print("The number is:", num)
+
+if(num<0):
+    print("Thenumber is negative")
+
+elif(num==0):
+    print("The number is zero")
+
+else:
+    print("The number is positive")
+
+
+
+
+
