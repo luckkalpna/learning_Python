a8d2f9673c90d2956c43c566a6d26ccb5d83a95f
luckkalpna <luckkalpna@gmail.com>
2024-06-26 13:17:10 +0530

    String Methods and Types

---
+++ string-type.py
@@ -0,0 +1,24 @@
+# fruit = "Mango"
+
+# # find length in any string
+
+# mangoLen = len(fruit)
+# print("Mango is a", mangoLen, "letter word.")
+
+# # Slicing is an string
+
+# print(fruit[0:4])
+# print(fruit[1:4])
+# print(fruit[:5])
+# print(fruit[:])
+
+str1 = "Hello Server"
+# print(str1)
+# print(str1.upper())  # Change to UpperCase
+# print(str1.lower())  # Chnage to LowerCase
+
+# Repalce method
+
+print(str1.replace("Server", "World"))
+
+
