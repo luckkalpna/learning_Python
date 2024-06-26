79d5f229163c9f5d1aafb3a7e0a4f5c57e68f142
luckkalpna <luckkalpna@gmail.com>
2024-06-26 20:45:59 +0530

    Continue & Break Statement

---
+++ break&continue.py
@@ -0,0 +1,25 @@
+# printing table of 5
+
+# for i in range(12):
+#         print("5 X", i+1, "=", 5*(i+1))
+
+
+
+# We using continue statement for skiping any statement
+
+# for i in range(12):
+#     if(i == 10):
+#         print('Skip the iteration')
+#         continue
+#     print("5 X", i, "=", 5*i)
+
+
+
+# ----------------------Break Statement----------------
+
+for i in range(12):
+    if(i ==10):
+        break
+    print("5 X", i, "=", 5*i)
+
+print("Braking out of the loop")
---
+++ while-loop.py
@@ -0,0 +1,16 @@
+# i = 0
+# while(i<5):
+#     print(i)
+#     i = i + 1
+
+# print("Done")
+
+# Taking input form user untill satisfies the condition in loop
+
+i = int(input("Enter any number:"))
+
+while(i<=38):
+    i = int(input("Number is less then try again:"))
+    print(i)
+else:
+    print("Well Done")
