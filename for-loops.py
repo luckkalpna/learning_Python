a392c54d1e05070cb5236e2b4cadeb577957e686
luckkalpna <luckkalpna@gmail.com>
2024-06-26 14:25:54 +0530

    For loop

---
+++ for-loops.py
@@ -0,0 +1,13 @@
+# colors = ["Red", "Yellow", "Green", "Blue", "Orange"]
+
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i)
+
+for k in range(5):
+    # print(k)
+    print(k+1)
+
+    # for i in range(1, 2001):
+    #     print(i)
