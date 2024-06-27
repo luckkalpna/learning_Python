0a533121b4147a7feda7df74688542da4fb6b70a
luckkalpna <luckkalpna@gmail.com>
2024-06-27 10:21:06 +0530

    List items

---
+++ function.py
@@ -0,0 +1,50 @@
+# def calculateGmean(a, b):
+#     mean = a*b/(a+b)
+#     print(mean)
+
+# def isGreater(a, b):
+#     if(a>b):
+#         print("First nubmer is grater")
+#     else:
+#         print("Second number is greater or equal")
+
+
+# For escape any undefined function
+
+# def isLess(a, b):
+#     pass
+
+
+# a = 9
+# b = 8
+# gmean1 = a*b/(a+b)
+# print(gmean1)
+# calculateGmean(a, b)
+# if(a>b):
+#     print("First nubmer is grater")
+# else:
+#     print("Second number is greater or equal")
+# isGreater(a, b)
+
+
+# c = 8
+# d = 16
+# gmean2 = c*d/(c+d)
+# print(gmean2)
+# calculateGmean(c, d)
+# if(a>b):
+#     print("First nubmer is grater")
+# else:
+#     print("Second number is greater or equal")
+# isGreater(c, d)
+
+# Using function as in tuple
+
+def averange(*numbers):
+    # print(type(numbers))
+    sum = 0
+    for i in numbers:
+        sum = sum + i
+    print("Average is:", sum/len(numbers))
+
+averange(2, 5, 8)
---
+++ lists.py
@@ -0,0 +1,41 @@
+list = [3,5,7,9,"New",11]
+# print(list)
+# print(type(list))
+# print(list[0])
+# print(list[1])
+# print(list[2])
+
+# -------------------Negative Indexing------------------
+# print(list[-3])
+# print(list[len(list)-3])
+# print(list[5-3])
+# print(list[2])
+
+
+# -----------------Finding any element in the list------------------
+
+# if 4 in list:
+#     print("Yes")
+# else:
+#     print("NO")
+
+
+# if "New" in list:
+#     print("Yes")
+# else:
+#     print("NO")
+
+
+# -------------------Comprehension list-----------------
+
+# marks1 = [i for i in range(5)]
+# print(marks1)
+
+# marks2 = [i*i for i in range(5)]
+# print(marks2)
+
+# -------------------Comprehension list using For if condition-----------------
+
+marks = [i for i in range(20) if(i%2==0)]
+print(marks)
+
