a5a3f6df6b03874f4d8e8326b65d9bd58689fddc
luckkalpna <luckkalpna@gmail.com>
2024-07-01 15:10:38 +0530

    List methods

--- lists.py
+++ lists.py
@@ -36,6 +36,18 @@ list = [3,5,7,9,"New",11]
 
 # -------------------Comprehension list using For if condition-----------------
 
-marks = [i for i in range(20) if(i%2==0)]
-print(marks)
+# marks = [i for i in range(20) if(i%2==0)]
+# print(marks)
 
+
+# ------------------------list methods-------------------------
+
+l =[11, 34, 3, 6, 32, 2, 9, 9]
+print(l)
+# l.append(100)  # For adding an element
+
+# l.sort(reverse=True)
+# l.reverse()
+# print(l.index(6))   # For finding any index position exist in list
+print(l.count(6))     # For count any item in the list
+# print(l)
