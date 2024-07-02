eccd092fa795178163d55e6faeff148027ac4b00
luckkalpna <luckkalpna@gmail.com>
2024-07-02 18:25:58 +0530

    dictionary methods

--- sets&dict.py
+++ sets&dict.py
@@ -20,19 +20,32 @@
 
 # ------------------------Dictionary in python--------------------
 
-dic = {
-    "name": "Karan",
-    "age": 20,
-    "eligible": True
-}
-# print(dic)
-# print(dic["name"])
-# print(dic.keys())
+# dic = {
+#     "name": "Karan",
+#     "age": 20,
+#     "eligible": True
+# }
+# # print(dic)
+# # print(dic["name"])
+# # print(dic.keys())
 
-# for key in dic.keys():
-#     print(f"The value of corrosponding to the key {key} is {dic[key]}")
+# # for key in dic.keys():
+# #     print(f"The value of corrosponding to the key {key} is {dic[key]}")
 
-print(dic.items())
+# print(dic.items())
 
-for key, value in dic.items():
-    print(f"The value of corrosponding to the keys {key} is {value}")
+# for key, value in dic.items():
+#     print(f"The value of corrosponding to the keys {key} is {value}")
+
+# -----------------------Methods in dict-------------------
+
+ep1 = {122: 45, 125: 67, 138: 45, 670: 97}
+ep2 = {534: 95, 445: 89}
+# ep1.update(ep2)     # update method
+# ep1.clear()         # for clear any dict
+ep1.popitem()         # for remove any last key in the liist
+print(ep1)
+
+
+
+
