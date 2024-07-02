139f1e964352024341d704579f67f3bae4724567
luckkalpna <luckkalpna@gmail.com>
2024-07-02 21:38:11 +0530

    import data form another file

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-print("Hello Server")
+# print("Hello Server")
 
-# Escape sequence character
+# # Escape sequence character
 
-print("Hello tgis is an \nEscape sequence character")
+# print("Hello tgis is an \nEscape sequence character")
+
+from name import welcome, aman        # import data from any other file
+import math
+result = math.sqrt(4)   
+# print(result)
+
+from math import sqrt, pi      # another way to use any method
+result = sqrt(4) * pi
+# print(result)
+
+print(dir(math))               # for access all the functions in any method
+welcome()
+print(aman)
---
+++ name.py
@@ -0,0 +1,4 @@
+def welcome():
+    print("Hey you are welcome my friend")
+
+aman = "Aman is a good boy"
