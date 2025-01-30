bd5c3e29956db48ed4c520590038c5416a2da0c1
luckkalpna <luckkalpna@gmail.com>
2025-01-30 22:11:13 +0530

    Decorators

--- oops.py
+++ oops.py
@@ -16,18 +16,35 @@
 
 # --------------------------------------------------------------------
 
-# Constructors
+# -----Constructors------
 
-class Person:
-  def __init__(self, n, o):
-    print("Hey I am a Person")
-    self.name = n
-    self.occ = o
+# class Person:
+#   def __init__(self, n, o):
+#     print("Hey I am a Person")
+#     self.name = n
+#     self.occ = o
 
-  def info(self):
-    print(f"{self.name} is a {self.occ}")
+#   def info(self):
+#     print(f"{self.name} is a {self.occ}")
 
-a = Person("Kalpna", "Developer")
-b = Person("Divya", "HR")
-a.info()
-b.info()
+# a = Person("Kalpna", "Developer")
+# b = Person("Divya", "HR")
+# a.info()
+# b.info()
+
+# --------------------------------------------------------------------
+
+# -------Decorators---------
+
+def greet(fx):
+  def mfx():
+    print("Good Morning")
+    fx()
+    print("Thanks for using this function")
+  return mfx
+  
+@greet
+def hello():
+  print("Hello World")
+
+hello()
