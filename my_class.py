03c81e526a6436b3619cb6e5d25b30e6ce4cd2a4
luckkalpna <luckkalpna@gmail.com>
2025-01-30 20:26:14 +0530

    Intoduce Classes and Objects

---
+++ my_class.py
@@ -0,0 +1,13 @@
+class Person:
+  name= "Kalpna"
+  occ= "Software Developer"
+  neworth = 10
+
+  # def info(self):
+  #   print(f"{self.name} is a {self.occ}")
+
+a = Person()
+
+a.name = "Ravi"
+a.occ = "Tester"
+print(a.name, a.occ)
