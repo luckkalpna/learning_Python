05fa2c0ad19a6cf10f774e8f701f7ec2757b01ea
luckkalpna <luckkalpna@gmail.com>
2025-01-30 21:37:51 +0530

    Constructors

---
+++ oops.py
@@ -0,0 +1,33 @@
+# ----Clasess and objects--------
+
+# class Person:
+#   name= "Kalpna"
+#   occ= "Software Developer"
+#   neworth = 10
+
+#   # def info(self):
+#   #   print(f"{self.name} is a {self.occ}")
+
+# a = Person()
+
+# a.name = "Ravi"
+# a.occ = "Tester"
+# print(a.name, a.occ)
+
+# --------------------------------------------------------------------
+
+# Constructors
+
+class Person:
+  def __init__(self, n, o):
+    print("Hey I am a Person")
+    self.name = n
+    self.occ = o
+
+  def info(self):
+    print(f"{self.name} is a {self.occ}")
+
+a = Person("Kalpna", "Developer")
+b = Person("Divya", "HR")
+a.info()
+b.info()
