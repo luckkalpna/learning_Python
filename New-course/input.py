2055824a58e6590e727b91189af0b854abc97e42
luckkalpna <luckkalpna@gmail.com>
2025-06-14 17:28:30 +0530

    Introduction to string

---
+++ New-course/input.py
@@ -0,0 +1,6 @@
+a = int(input("Enter number a: "))
+b = int(input("Enter number b: "))
+
+print("Number a is: ", a)
+print("Number b is: ", b)
+print("Sum of (a + b) is: ", a+b)
---
+++ New-course/intro_to_strings.py
@@ -0,0 +1,9 @@
+name = "Kalpana"
+
+nameshort = name[0:3]
+print(nameshort)
+character1 = name[3]
+print(character1)
+
+# print(name[-4:-1])
+# print(name[1:4])
