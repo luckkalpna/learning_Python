a3b110bf19fcd7a1aa06b669de4b16c76ae6b29a
luckkalpna <luckkalpna@gmail.com>
2025-06-14 16:42:35 +0530

    Operators

---
+++ New-course/operators.py
@@ -0,0 +1,37 @@
+# Arithmetic Operators
+a = 7 
+b = 4
+c = a+b
+print(c)
+
+# Assignments Operators
+a = 4-2
+print(a)
+b = 6
+b += 3
+print(b)
+
+# Comparision Operators
+# d = 5<4
+# d = 5!=7
+d = 5==5
+print(d)
+
+# Logical Operators
+e = True or False
+
+# Truth table for 'or'
+print("True or False is:", True or False)
+print("False or True is:", False or True)
+print("True or True is:", True or True)
+print("False or False is:", False or False)
+
+# Truth table for 'and'
+print("True and False is:", True and False)
+print("False and True is:", False and True)
+print("True and True is:", True and True)
+print("False and False is:", False and False)
+
+# "not" operator
+print(not(True))
+print(not(False))
