41556da8bef3a429d2aabc636338c498415d0352
luckkalpna <luckkalpna@gmail.com>
2025-06-15 14:44:23 +0530

    Conditionals

---
+++ New-course/conditionals.py
@@ -0,0 +1,10 @@
+a = int(input("Enter your age: "))
+
+if(a>=18):
+  print("Your are above the age of consent")
+  print("Good To Go")
+
+else:
+  print("Your are below the age of consent")
+
+print("End of Program")
