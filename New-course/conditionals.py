fb40f7d2e0ca8be9c2cbfb2ea7c906c319f3edb3
luckkalpna <luckkalpna@gmail.com>
2025-06-15 14:52:59 +0530

    If else condition & ifelse ladder

--- New-course/conditionals.py
+++ New-course/conditionals.py
@@ -1,8 +1,21 @@
 a = int(input("Enter your age: "))
 
+# if(a>=18):
+#   print("Your are above the age of consent")
+#   print("Good To Go")
+
+# else:
+#   print("Your are below the age of consent")
+
 if(a>=18):
   print("Your are above the age of consent")
   print("Good To Go")
+
+elif(a<0):
+  print("You are entering an invalid negative age")
+
+elif(a==0):
+  print("Your are entering 0 which is not a valid age")
 
 else:
   print("Your are below the age of consent")
